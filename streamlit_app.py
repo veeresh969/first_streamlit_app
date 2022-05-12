dfca56d75e30e4bc963e2dba6402c731bd3d18de
veeresh969 <104963788+veeresh969@users.noreply.github.com>
2022-05-12 16:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_data_row = my_cur.fetchone()
